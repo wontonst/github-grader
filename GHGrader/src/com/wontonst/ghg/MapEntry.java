da965c7c2c544273c1cab3acbaa80cb0f8638e96
Yiwei Zheng <yiweizhe@usc.edu>
2013-10-23 16:47:00 -0700

    tests and more code

---
+++ GHGrader/src/com/wontonst/ghg/MapEntry.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wontonst.ghg;
+
+/**
+ *
+ * @author RoyZheng
+ */
+public class MapEntry {
+
+    public String key;
+    public String value;
+
+    public MapEntry(String k, String v) {
+        this.key = k;
+        this.value = v;
+    }
+}
---
+++ GHGrader/test/com/wontonst/ghg/GHGFilePatternsTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wontonst.ghg;
+
+import java.util.regex.Pattern;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author RoyZheng
+ */
+public class GHGFilePatternsTest {
+
+    public GHGFilePatternsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testHeaderMarker() {
+        Pattern p = GHGFilePatterns.header_marker;
+        Assert.assertTrue(("---").matches(p.pattern()));
+        Assert.assertFalse(("--").matches(p.pattern()));
+    }
+
+    @Test
+    public void testHeaderTitle() {
+    }
+}
