9884085300ccac35171437f6ad66f31a5552431d
Yiwei Zheng <yiweizhe@usc.edu>
2013-10-28 20:09:13 -0700

    adding supporting files

---
+++ GHGrader/src/com/wontonst/ghg/file/Format.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wontonst.ghg.file;
+
+/**
+ *
+ * @author RoyZheng
+ */
+public enum Format {
+
+    GHG, MD, HTML, JEKYLL_MD, JEKYLL_HTML
+}
