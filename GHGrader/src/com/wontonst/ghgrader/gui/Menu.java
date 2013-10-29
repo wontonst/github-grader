20e9144fd454421452e6ba371520806df736445d
Yiwei Zheng <yiweizhe@usc.edu>
2013-10-29 09:26:53 -0700

    adding menu

---
+++ GHGrader/src/com/wontonst/ghgrader/gui/Menu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wontonst.ghgrader.gui;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+/**
+ *
+ * @author RoyZheng
+ */
+public class Menu extends JMenuBar {
+
+    protected JMenu file;
+
+    public Menu() {
+        super();
+    }
+}
