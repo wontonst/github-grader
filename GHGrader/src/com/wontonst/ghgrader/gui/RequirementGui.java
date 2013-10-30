d5d8e926932b8c2a5c9a4e3be56112e840770d93
Yiwei Zheng <yiweizhe@usc.edu>
2013-10-29 19:15:02 -0700

    backend model files

---
+++ GHGrader/src/com/wontonst/ghgrader/gui/RequirementGui.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wontonst.ghgrader.gui;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author RoyZheng
+ */
+public class RequirementGui extends JPanel{
+    
+    public RequirementGui(){
+        
+    }
+}
---
+++ GHGrader/src/com/wontonst/ghgrader/model/Project.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wontonst.ghgrader.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author RoyZheng
+ */
+public class Project {
+    String raw_ghg;
+    
+    List<Student> students;
+}
