39192d4a1514b3a46e63c20a2e4644fffda2e185
Yiwei Zheng <yiweizhe@usc.edu>
2013-11-03 00:56:22 -0700

    trying to fix bug

--- GHGrader/src/com/wontonst/ghgrader/gui/ProjectGui.java
+++ GHGrader/src/com/wontonst/ghgrader/gui/ProjectGui.java
@@ -11,7 +11,10 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 
 /**
  * Displays the grades for a project.
@@ -22,6 +25,9 @@ public class ProjectGui extends JPanel {
 
     Project project;///<model representation
     List<StudentGui> students = new ArrayList<StudentGui>();
+    
+    JButton next, previous;
+    
 
     public ProjectGui(Project p, List<StudentGui> gui) {
         this.project = p;
@@ -34,6 +40,13 @@ public class ProjectGui extends JPanel {
 
         for (StudentGui s : gui) {
             this.add(s);
+            gbc.gridy += 1;
         }
+        this.add(new JSeparator(SwingConstants.HORIZONTAL));
+        this.next = new JButton("Next");
+        this.previous = new JButton("Previous");
+        this.add(this.next);
+        gbc.gridx = 1;
+        this.add(this.previous);
     }
 }
